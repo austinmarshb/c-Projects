d149d02520b4c605ba087d30b423fb7458f30aa9
Austin Marsh <168210295+austinmarshb@users.noreply.github.com>
2024-11-14 10:42:16 -0500

    Update fizzBuzz.cs

--- fizzBuzz.cs
+++ fizzBuzz.cs
@@ -1,45 +1,31 @@
+using System;
 
-//concatenation method
-public void FizzBuzz()
+namespace FizzingBuzzer
 {
-    for (int i = 1; i <= 100; i++)
+    public class Program
     {
-        string output = "";
+        public static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.FizzBuzz();
+        }
 
-        if (i % 3 == 0)
-            output += "Fizz";
-        if (i % 5 == 0)
-            output += "Buzz";
+        public void FizzBuzz()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                string output = "";
 
-        if (output == "")
-            output = i.ToString();
+                if (i % 3 == 0)
+                    output += "Fizz";
+                if (i % 5 == 0)
+                    output += "Buzz";
 
-        Console.WriteLine(output);
+                if (output == "")
+                    output = i.ToString();
+
+                Console.WriteLine(output);
+            }
+        }
     }
 }
-
-//the first try i did with C#
-int[] arr = Enumerable.Range(1, 100).ToArray();
-
-for (int i = 0; i < arr.Length; i++)
-{
-    if (arr[i] % 3 == 0 && arr[i] % 5 == 0)
-    {
-        Console.WriteLine("FizzBuzz");
-    }
-    else if (arr[i] % 3 == 0)
-    {
-        Console.WriteLine("Fizz");
-    }
-    else if (arr[i] % 5 == 0)
-    {
-        Console.WriteLine("Buzz");
-    }
-    else
-    {
-        Console.WriteLine(arr[i]);
-    }
-}
-
-Console.ReadKey();
-
