7ae3da64ae95bb6c978977708ed03c5e70b0b3d9
Austin Marsh <168210295+austinmarshb@users.noreply.github.com>
2024-11-14 10:32:25 -0500

    Update fizzBuzz.cs

--- fizzBuzz.cs
+++ fizzBuzz.cs
@@ -1,3 +1,24 @@
+
+//concatenation method
+public void FizzBuzz()
+{
+    for (int i = 1; i <= 100; i++)
+    {
+        string output = "";
+
+        if (i % 3 == 0)
+            output += "Fizz";
+        if (i % 5 == 0)
+            output += "Buzz";
+
+        if (output == "")
+            output = i.ToString();
+
+        Console.WriteLine(output);
+    }
+}
+
+//the first try i did with C#
 int[] arr = Enumerable.Range(1, 100).ToArray();
 
 for (int i = 0; i < arr.Length; i++)
@@ -21,3 +42,4 @@ for (int i = 0; i < arr.Length; i++)
 }
 
 Console.ReadKey();
+
