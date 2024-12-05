eaf7b409ef61655d5513b39479d5d5f1ab86eef6
Austin Marsh <168210295+austinmarshb@users.noreply.github.com>
2024-12-05 11:43:49 -0500

    Update fizzBuzz.cs

--- fizzBuzz.cs
+++ fizzBuzz.cs
@@ -21,7 +21,6 @@ namespace FizzingBuzzer
                     output += "Fizz";
                 if (i % 5 == 0)
                     output += "Buzz";
-
                 if (output == "")
                     output = i.ToString();
 
